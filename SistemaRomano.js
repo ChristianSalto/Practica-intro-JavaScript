224c4baaa0b593a138ce2450274ab1e0118f7bc9
Christian Salto <romeosaltovk@gmail.com>
2019-12-16 17:43:05 +0100

    Fin Kata 2

--- SistemaRomano.js
+++ SistemaRomano.js
@@ -1,9 +1,50 @@
-const numberRomanArab = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+const numberRomanArab = {
+    "": 0,
+    I: 1, II: 2, III: 3, IV: 4, V: 5, VI: 6, VII: 7, VIII: 8, IX: 9, X: 10,
+    XX: 20, XXX: 30, XL: 40, L: 50, LX: 60, LXX: 70, LXXX: 80, XC: 90, C: 100, CC: 200,
+    CCC: 300, CD: 400, D: 500, DC: 600, DCC: 700, DCCC: 800, CM: 900, M: 1000, MM: 2000, MMM: 3000,
+
+    getKeys(keys) {
+        for (let key in numberRomanArab) {
+            if (numberRomanArab[key] == keys) {
+                return key;
+            }
+        }
+    }
+};
 
 let romanToArab = numberRoman => {
-
-
-
+    let sumRoman = "";
+    let numberString = "";
+    let numberInt = 0;
+    let numberLength = numberRoman.toString().length;
+    numberString = numberRoman.toString();
+    for (let x = 0; x < numberLength; x++) {
+        for (let i of numberString) {
+            if (numberLength === 4) {
+                numberInt = parseInt(i);
+                sumRoman += numberRomanArab.getKeys(numberInt * 1000);
+                numberString = numberString.slice(1);
+                numberLength = numberString.length;
+            } else if (numberLength === 3) {
+                numberInt = parseInt(i);
+                sumRoman += numberRomanArab.getKeys(numberInt * 100);
+                numberString = numberString.slice(1);
+                numberLength = numberString.length;
+            } else if (numberLength === 2) {
+                numberInt = parseInt(i);
+                sumRoman += numberRomanArab.getKeys(numberInt * 10);
+                numberString = numberString.slice(1);
+                numberLength = numberString.length;
+            } else {
+                numberInt = parseInt(i);
+                sumRoman += numberRomanArab.getKeys(numberInt);
+                numberString = numberString.slice(1);
+                numberLength = numberString.length;
+            }
+        }
+    }
+    console.log(sumRoman)
 }
 
 let arabToRoman = numberArab => {
@@ -50,8 +91,11 @@ let arabToRoman = numberArab => {
     } else {
         return console.log("Sorry, the number could not be translated");
     }
-
-    console.log(sumaArab)
+    if (sumaArab < 4000) {
+        console.log(sumaArab);
+    } else {
+        console.log("Only numbers smallest of 4000, this number is more higher")
+    }
 }
 
 let firstAndSecondRule = numberToValidate => {
@@ -97,7 +141,7 @@ let fourthRule = numberToValidate => {
         for (positionLetter; positionLetter < numberToValidate.length; positionLetter++) {
             if (i === "V" || i === "L" || i === "D") {
                 if (positionLetter != 0) {
-                    if (valueNumber <= numberRomanArab[numberToValidate[positionLetter]]) {
+                    if (valueNumber < numberRomanArab[numberToValidate[positionLetter + 1]]) {
                         return console.log("The symbols V, L and D cannot be placed to the left of another higher");
                     }
                 }
@@ -152,7 +196,8 @@ let repetitiveNumber = (charAt, numberToValidate) => {
 }
 
 
-arabToRoman("vm");
+arabToRoman("MCCCXXXV");
+romanToArab(1335);
 
 /*
 # Segunda Kata
